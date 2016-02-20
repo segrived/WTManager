e9254a1515ed02bd4cba2f3367d9674deab82214
Babaev Evgeniy <segrived@gmail.com>
2016-02-20 13:12:35 +0200

    Move all forms to UI namespace

---
+++ WTManager/Controls/MyToolStripMenuRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WTManager.Controls
+{
+    class MyColorTable : ProfessionalColorTable
+    {
+        public override Color ImageMarginGradientBegin => Color.White;
+        public override Color ImageMarginGradientMiddle => Color.White;
+        public override Color ImageMarginGradientEnd => Color.White;
+    }
+
+    class MyToolStripMenuRenderer : ToolStripProfessionalRenderer
+    {
+        public MyToolStripMenuRenderer() : base(new MyColorTable()) { }
+
+        protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs e) {
+            if (e.Item.Enabled)
+                base.OnRenderMenuItemBackground(e);
+        }
+
+        protected override void OnRenderButtonBackground(ToolStripItemRenderEventArgs e) {
+            if (e.Item.Enabled)
+                base.OnRenderMenuItemBackground(e);
+        }
+    }
+}
--- WTManager/Program.cs
+++ WTManager/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Windows.Forms;
 using WTManager.Helpers;
+using WTManager.UI;
 
 namespace WTManager
 {
