e62b7429fd23d803b40b1584479efdba3d3dee22
Babaev Evgeniy <segrived@gmail.com>
2016-02-21 07:27:01 +0200

    Open log viewer windows in center by default

--- WTManager/UI/LogFileViewerForm.Designer.cs
+++ WTManager/UI/LogFileViewerForm.Designer.cs
@@ -25,7 +25,6 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogFileViewerForm));
             this.logFileContent = new System.Windows.Forms.RichTextBox();
             this.logFileContentWrapper = new System.Windows.Forms.Panel();
             this.logFileContentWrapper.SuspendLayout();
@@ -39,29 +38,30 @@
             this.logFileContent.Location = new System.Drawing.Point(0, 0);
             this.logFileContent.Name = "logFileContent";
             this.logFileContent.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.logFileContent.Size = new System.Drawing.Size(642, 376);
+            this.logFileContent.Size = new System.Drawing.Size(622, 406);
             this.logFileContent.TabIndex = 2;
             this.logFileContent.Text = "";
             // 
             // logFileContentWrapper
             // 
-            this.logFileContentWrapper.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.logFileContentWrapper.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.logFileContentWrapper.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.logFileContentWrapper.Controls.Add(this.logFileContent);
-            this.logFileContentWrapper.Location = new System.Drawing.Point(12, 12);
+            this.logFileContentWrapper.Location = new System.Drawing.Point(14, 14);
             this.logFileContentWrapper.Name = "logFileContentWrapper";
-            this.logFileContentWrapper.Size = new System.Drawing.Size(644, 378);
+            this.logFileContentWrapper.Size = new System.Drawing.Size(624, 408);
             this.logFileContentWrapper.TabIndex = 3;
             // 
-            // LogFileViewer
+            // LogFileViewerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 402);
+            this.ClientSize = new System.Drawing.Size(652, 436);
             this.Controls.Add(this.logFileContentWrapper);
-            this.Name = "LogFileViewer";
+            this.Name = "LogFileViewerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LogFileViewer";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LogFileViewer_FormClosing);
             this.Load += new System.EventHandler(this.LogFileViewer_Load);
