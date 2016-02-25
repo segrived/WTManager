a0920e06dbae20b3eeb29ae482addfaa9e3724c8
Babaev Evgeniy <segrived@gmail.com>
2016-02-25 11:54:54 +0200

    Fix file architecture detection

--- WTManager/Properties/AssemblyInfo.cs
+++ WTManager/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("9fad443f-f504-47fa-802c-6470413097d0")]
 
-[assembly: AssemblyVersion("0.1.223.5895")]
-[assembly: AssemblyFileVersion("0.1.223.5895")]
+[assembly: AssemblyVersion("0.1.224.5899")]
+[assembly: AssemblyFileVersion("0.1.224.5899")]
